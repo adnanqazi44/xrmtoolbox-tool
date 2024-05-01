59e93e52d75e39d55a29b22608f249dc89b6c576
Adn Qazi <86150526+adnanqazi44@users.noreply.github.com>
2024-05-01 11:00:36 +0500

    Add files via upload

---
+++ MyPluginControl.cs
@@ -0,0 +1,361 @@
+﻿using McTools.Xrm.Connection;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+
+namespace MSToolByAdn
+{
+    public partial class MyPluginControl : PluginControlBase
+    {
+        string filepath = null;
+        public List<LoadSolutionsList> LSList = new List<LoadSolutionsList>();
+        private Settings mySettings;
+
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void tsbSample_Click(object sender, EventArgs e)
+        {
+            // The ExecuteMethod method handles connecting to an
+            // organization if XrmToolBox is not yet connected
+            ExecuteMethod(GetAccounts);
+        }
+
+        private void GetAccounts()
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Getting accounts",
+                Work = (worker, args) =>
+                {
+                    args.Result = Service.RetrieveMultiple(new QueryExpression("account")
+                    {
+                        TopCount = 50
+                    });
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+                    if (result != null)
+                    {
+                        MessageBox.Show($"Found {result.Entities.Count} accounts");
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        public LoadSolutionsList GetSelectedValue(string SelectedSolutionName)
+        {
+            return LSList.FirstOrDefault(x => x.SolutionName == SelectedSolutionName);
+        }
+        public LoadSolutionsList Value()
+        {
+            return LSList.FirstOrDefault();
+        }
+        private async void LoadSolutionButton_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                control.Enabled = false;
+            }
+            //var loaderForm = new Form1();
+
+            //loaderForm.Show(this);
+
+            LoadSolutionButton.Text = "Loading...";
+            // WaitingObj.Show(this);
+            //DirectoryButton.Enabled = false;
+            //DirectoryTBox.ReadOnly = true;
+            //ExtractSolution.Enabled = true;
+            //SearchStringKeyword.ReadOnly = true;
+            //ClearButton.Enabled = false;
+            //SearchButton.Enabled = false;
+            await Task.Delay(2000);
+
+            var query_ismanaged = false;
+            var query_isvisible = true;
+            var query_uniquename = "default";
+            var solutionQuery = new QueryExpression("solution");
+            solutionQuery.ColumnSet = new ColumnSet("friendlyname", "uniquename", "solutionid", "version");
+            solutionQuery.Criteria.AddCondition("ismanaged", ConditionOperator.Equal, query_ismanaged);
+            solutionQuery.Criteria.AddCondition("isvisible", ConditionOperator.Equal, query_isvisible);
+            solutionQuery.Criteria.AddCondition("friendlyname", ConditionOperator.NotEqual, query_uniquename);
+            EntityCollection solutionCollection = Service.RetrieveMultiple(solutionQuery);
+            SolutionComboBox.Items.Clear();
+            foreach (Entity solution in solutionCollection.Entities)
+            {
+                var solutionId = solution.GetAttributeValue<Guid>("solutionid");
+                var solutionName = solution.GetAttributeValue<string>("friendlyname");
+                var uniquename = solution.GetAttributeValue<string>("uniquename");
+                var version = solution.GetAttributeValue<string>("version");
+                LSList.Add(new LoadSolutionsList { SolutionId = solutionId, Version = version, SolutionName = solutionName, UniqueName = uniquename });
+                SolutionComboBox.Items.Add(solutionName);
+            }
+
+            //loaderForm.Close();
+            foreach (Control control in this.Controls)
+            {
+                control.Enabled = true;
+            }
+            LoadSolutionButton.Text = "Load Solutions";
+            //DirectoryButton.Enabled = true;
+            //DirectoryTBox.ReadOnly = false;
+            //ExtractSolution.Enabled = true;
+        }
+
+        private void ExtractSolutionButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Control control in this.Controls)
+                {
+                    control.Enabled = false;
+                }
+                //var loaderForm = new LoadingForm();
+                //loaderForm.ShowDialog();
+
+                var selectedItem = GetSelectedValue(SolutionComboBox.SelectedItem.ToString());
+                var selectedSolution = selectedItem.SolutionName.ToString();
+                var uniqueName = selectedItem.UniqueName.ToString();
+
+                if (selectedSolution == null)
+                {
+                    //WaitingObj.Close();
+                    return;
+                }
+                var solutionId = selectedItem.SolutionId.ToString();
+                byte[] solutionFile = RetrieveSolutionFile(Service, uniqueName);
+                DateTime currentDateTime = DateTime.Now;
+                string randomNumber = currentDateTime.ToString("yyyyMMddHHmmss");
+
+                if (!string.IsNullOrEmpty(DirectoryTextBox.Text))
+                {
+                    string selectedDirectory = DirectoryTextBox.Text;
+                    string solutionFileName = selectedSolution + randomNumber + ".zip";
+                    string solutionFilePath = Path.Combine(selectedDirectory, solutionFileName);
+                    File.WriteAllBytes(solutionFilePath, solutionFile);
+                    LogInfo("Solution '{0}' exported to: {1}", selectedSolution, solutionFilePath);
+                    string extractionFolderPath = Path.Combine(Path.GetDirectoryName(solutionFilePath), Path.GetFileNameWithoutExtension(solutionFilePath));
+                    UnzipSolution(solutionFilePath, extractionFolderPath);
+                    filepath = extractionFolderPath;
+                    // MessageBox.Show("File saved to: " + solutionFilePath);
+                    //SearchStringKeyword.ReadOnly = false;
+                    //SearchButton.Enabled = true;
+                    //ClearButton.Enabled = true;
+                }
+                else
+                {
+                    //WaitingObj.Close();
+                    MessageBox.Show("Please select a directory first.");
+                }
+
+
+                //loaderForm.Close();
+                foreach (Control control in this.Controls)
+                {
+                    control.Enabled = true;
+                }
+                MessageBox.Show("Operation completed successfully!");
+            }
+            catch (Exception ex)
+            {
+                // WaitingObj.Close();
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        public byte[] RetrieveSolutionFile(IOrganizationService service, string solutionUniqueName)
+        {
+            ExportSolutionRequest exportRequest = new ExportSolutionRequest
+            {
+                SolutionName = solutionUniqueName,
+                Managed = false,
+                ExportAutoNumberingSettings = true,
+                ExportCalendarSettings = true,
+                ExportCustomizationSettings = true,
+                ExportEmailTrackingSettings = true,
+                ExportExternalApplications = true,
+                ExportGeneralSettings = true,
+                ExportMarketingSettings = true,
+                ExportOutlookSynchronizationSettings = true,
+                ExportRelationshipRoles = true,
+                ExportSales = true
+            };
+            ExportSolutionResponse exportResponse = (ExportSolutionResponse)service.Execute(exportRequest);
+            byte[] exportXml = exportResponse.ExportSolutionFile;
+            return exportXml;
+        }
+
+        public void UnzipSolution(string zipFilePath, string extractionFolderPath)
+        {
+            if (zipFilePath != null)
+            {
+                ZipFile.ExtractToDirectory(zipFilePath, extractionFolderPath);
+                Console.WriteLine("Solution extracted successfully to: " + extractionFolderPath);
+            }
+            return;
+        }
+
+        private void DirectoryButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            DialogResult result = folder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                DirectoryTextBox.Text = folder.SelectedPath;
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string Keyworddata = SearchKeywordTextBox.Text;
+            SearchForStringInJsonFiles(filepath, Keyworddata);
+            SolutionComboBox.Items.Clear();
+            DirectoryTextBox.Text = "";
+        }
+
+        public void SearchForStringInJsonFiles(string extractionFolderPath, string searchString)
+        {
+            ResultTextBox.Clear();
+            string workflowFolderPath = Path.Combine(extractionFolderPath, "Workflows");
+            if (!Directory.Exists(workflowFolderPath))
+            {
+                MessageBox.Show("Workflows folder not found.");
+                return;
+            }
+            var jsonFiles = Directory.GetFiles(workflowFolderPath, "*.json", SearchOption.AllDirectories);
+            foreach (var jsonFilePath in jsonFiles)
+            {
+                string jsonContent = File.ReadAllText(jsonFilePath);
+                JObject jsonObject = JObject.Parse(jsonContent);
+                if (jsonObject.ToString().Contains(searchString))
+                {
+                    string fileName = Path.GetFileName(jsonFilePath);
+                    ResultTextBox.AppendText(fileName + Environment.NewLine);
+                    ResultTextBox.Multiline = true;
+                    ResultTextBox.ScrollBars = ScrollBars.Vertical;
+                    //MessageBox.Show($"String '{searchString}' found in JSON file: {jsonFilePath}");
+                }
+            }
+
+            if (jsonFiles.Length == 0)
+            {
+                MessageBox.Show("No JSON files found in the Workflows folder.");
+            }
+            else
+            {
+                MessageBox.Show("Search completed in all JSON files within the Workflows folder.");
+            }
+        }
+
+        private void ExportResultButton_Click(object sender, EventArgs e)
+        {
+            string textToExport = ResultTextBox.Text;
+            if (string.IsNullOrEmpty(textToExport))
+            {
+                MessageBox.Show("No Data For text export.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        writer.Write(textToExport);
+                    }
+
+                    MessageBox.Show("Text exported to file successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ResultTextBox.Clear();
+        }
+    }
+
+    public class LoadSolutionsList
+    {
+        public Guid SolutionId { get; set; }
+        public string SolutionName { get; set; }
+        public string UniqueName { get; set; }
+        public string Version { get; set; }
+    }
+}
---
+++ MyPluginControl.designer.cs
@@ -0,0 +1,250 @@
+﻿namespace MSToolByAdn
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbSample = new System.Windows.Forms.ToolStripButton();
+            this.LoadSolutionButton = new System.Windows.Forms.Button();
+            this.DirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.DirectoryButton = new System.Windows.Forms.Button();
+            this.ExtractSolutionButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ExportResultButton = new System.Windows.Forms.Button();
+            this.SearchKeywordTextBox = new System.Windows.Forms.TextBox();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SolutionComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.toolStripMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.tsbSample});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(826, 25);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(86, 22);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbSample
+            // 
+            this.tsbSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSample.Name = "tsbSample";
+            this.tsbSample.Size = new System.Drawing.Size(46, 22);
+            this.tsbSample.Text = "Try me";
+            this.tsbSample.Click += new System.EventHandler(this.tsbSample_Click);
+            // 
+            // LoadSolutionButton
+            // 
+            this.LoadSolutionButton.Location = new System.Drawing.Point(3, 29);
+            this.LoadSolutionButton.Name = "LoadSolutionButton";
+            this.LoadSolutionButton.Size = new System.Drawing.Size(93, 23);
+            this.LoadSolutionButton.TabIndex = 5;
+            this.LoadSolutionButton.Text = "Load Solution";
+            this.LoadSolutionButton.UseVisualStyleBackColor = true;
+            this.LoadSolutionButton.Click += new System.EventHandler(this.LoadSolutionButton_Click);
+            // 
+            // DirectoryTextBox
+            // 
+            this.DirectoryTextBox.Location = new System.Drawing.Point(100, 118);
+            this.DirectoryTextBox.Name = "DirectoryTextBox";
+            this.DirectoryTextBox.Size = new System.Drawing.Size(193, 20);
+            this.DirectoryTextBox.TabIndex = 0;
+            // 
+            // DirectoryButton
+            // 
+            this.DirectoryButton.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DirectoryButton.Location = new System.Drawing.Point(295, 117);
+            this.DirectoryButton.Name = "DirectoryButton";
+            this.DirectoryButton.Size = new System.Drawing.Size(25, 23);
+            this.DirectoryButton.TabIndex = 6;
+            this.DirectoryButton.Text = "...";
+            this.DirectoryButton.UseVisualStyleBackColor = true;
+            this.DirectoryButton.Click += new System.EventHandler(this.DirectoryButton_Click);
+            // 
+            // ExtractSolutionButton
+            // 
+            this.ExtractSolutionButton.Location = new System.Drawing.Point(3, 149);
+            this.ExtractSolutionButton.Name = "ExtractSolutionButton";
+            this.ExtractSolutionButton.Size = new System.Drawing.Size(93, 23);
+            this.ExtractSolutionButton.TabIndex = 7;
+            this.ExtractSolutionButton.Text = "Extract Solution";
+            this.ExtractSolutionButton.UseVisualStyleBackColor = true;
+            this.ExtractSolutionButton.Click += new System.EventHandler(this.ExtractSolutionButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(3, 221);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 8;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // ExportResultButton
+            // 
+            this.ExportResultButton.Location = new System.Drawing.Point(349, 221);
+            this.ExportResultButton.Name = "ExportResultButton";
+            this.ExportResultButton.Size = new System.Drawing.Size(78, 23);
+            this.ExportResultButton.TabIndex = 9;
+            this.ExportResultButton.Text = "Export Result";
+            this.ExportResultButton.UseVisualStyleBackColor = true;
+            this.ExportResultButton.Click += new System.EventHandler(this.ExportResultButton_Click);
+            // 
+            // SearchKeywordTextBox
+            // 
+            this.SearchKeywordTextBox.Location = new System.Drawing.Point(100, 185);
+            this.SearchKeywordTextBox.Name = "SearchKeywordTextBox";
+            this.SearchKeywordTextBox.Size = new System.Drawing.Size(193, 20);
+            this.SearchKeywordTextBox.TabIndex = 10;
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(3, 250);
+            this.ResultTextBox.Multiline = true;
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(424, 186);
+            this.ResultTextBox.TabIndex = 11;
+            // 
+            // SolutionComboBox
+            // 
+            this.SolutionComboBox.FormattingEnabled = true;
+            this.SolutionComboBox.Location = new System.Drawing.Point(100, 69);
+            this.SolutionComboBox.Name = "SolutionComboBox";
+            this.SolutionComboBox.Size = new System.Drawing.Size(220, 21);
+            this.SolutionComboBox.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Select Solution";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Extract Solution to";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Keyword to Search";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(0, 442);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 16;
+            this.ClearButton.Text = "Clear Result";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // MyPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SolutionComboBox);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.SearchKeywordTextBox);
+            this.Controls.Add(this.ExportResultButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.ExtractSolutionButton);
+            this.Controls.Add(this.DirectoryButton);
+            this.Controls.Add(this.DirectoryTextBox);
+            this.Controls.Add(this.LoadSolutionButton);
+            this.Controls.Add(this.toolStripMenu);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(826, 465);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.Button LoadSolutionButton;
+        private System.Windows.Forms.TextBox DirectoryTextBox;
+        private System.Windows.Forms.Button DirectoryButton;
+        private System.Windows.Forms.Button ExtractSolutionButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button ExportResultButton;
+        private System.Windows.Forms.TextBox SearchKeywordTextBox;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.ComboBox SolutionComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
